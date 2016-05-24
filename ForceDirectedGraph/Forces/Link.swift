28753a12234b6010472cb4ac775dfad93d246cf0
FredrikSjoberg <fredrik.sjoberg.swe@gmail.com>
2016-05-24 23:44:33 +0200

    closures for specifying spring

--- ForceDirectedGraph/Forces/Link.swift
+++ ForceDirectedGraph/Forces/Link.swift
@@ -11,15 +11,25 @@ import CoreGraphics
 
 public class Link: Force {
     
-    private(set) public var springLength: CGFloat = 50
+    private(set) public var springLength: (Edge) -> CGFloat = { _ in 50 }
     public func springLength(value: CGFloat) -> Link {
-        springLength = value
+        springLength = { _ in value }
         return self
     }
     
-    private(set) public var springCoefficient: CGFloat = 0.0002
+    public func springLength(closure: (Edge) -> CGFloat) -> Link {
+        springLength = closure
+        return self
+    }
+    
+    
+    private(set) public var springCoefficient: (Edge) -> CGFloat = { _ in 0.0002 }
     public func springCoefficient(value: CGFloat) -> Link {
-        springCoefficient = value
+        springCoefficient = { _ in value }
+        return self
+    }
+    public func springCoefficient(closure: (Edge) -> CGFloat) -> Link {
+        springCoefficient = closure
         return self
     }
     
@@ -35,8 +45,8 @@ extension Link {
         edges.forEach{
             let d = ($0.to.position == $0.from.position) ? $0.from.position.randomize(0.1) : $0.to.position - $0.from.position
             
-            let displacement = d.magnitude - springLength / $0.weight
-            let coeff = springCoefficient * displacement / d.magnitude
+            let displacement = d.magnitude - springLength($0) / $0.weight
+            let coeff = springCoefficient($0) * displacement / d.magnitude
             let force = d * coeff * 0.5
             
             $0.from.force = $0.from.force + force
